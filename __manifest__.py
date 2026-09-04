32e06e2644aae6fa237361e971af74d0dd5342a3
TotalPay <info@totalpay.global>
2026-09-04 20:07:14 +0400

    Remove 'website_sale' dependency from manifest

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@ Docs: https://docs.totalpay.global/
     """,
     'author': "TotalPay",
     'website': "https://totalpay.global",
-    'depends': ['payment','website_sale'],
+    'depends': ['payment'],
     'data': [
         'views/payment_provider_views.xml',
 
